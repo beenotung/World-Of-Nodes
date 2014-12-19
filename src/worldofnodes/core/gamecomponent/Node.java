c6feb8657c54663bb4aca3d643425c92c7d3cbfb
Beeno Tung <aabbcc1241@yahoo.com.hk>
2014-12-19 21:55:37 +0800

    working on Node

--- src/worldofnodes/core/gamecomponent/Node.java
+++ src/worldofnodes/core/gamecomponent/Node.java
@@ -1,41 +1,31 @@
 package worldofnodes.core.gamecomponent;
 
 import myutils.Utils;
-import myutils.Vector2D;
+import myutils.maths.graph.Vertex;
 
 import java.awt.*;
 import java.util.ArrayList;
 
-public class Node implements Cloneable {
-        public Vector2D location;
+public class Node {
+    public Vertex location;
     public ArrayList<Node> neighbours;
     public Color color;
-    public int width, height;
+    public float radius;
 
-    public Node(int width, int height, Color color, Vector2D location) {
-        this.width = width;
-        this.height = height;
+    public Node(float radius, Color color, Vertex location) {
+        this.radius = radius;
         this.color = color;
         this.location = location;
-        neighbours = new ArrayList<>();
+        neighbours = new ArrayList();
     }
 
-    public Node(int width, int height, Color color, Vector2D location, ArrayList<Node> neighbours) {
-        this.width = width;
-        this.height = height;
+    public Node(float radius, Color color, Vertex location, ArrayList<Node> neighbours) {
+        this.radius = radius;
         this.color = color;
         this.location = location;
         this.neighbours = neighbours;
     }
-
-    public Node clone() throws CloneNotSupportedException {
-        // Vertex result = new Vertex(this.width, this.height,
-        // this.color,this.location.clone(),(ArrayList<Vertex>)
-        // this.neighbours.clone());
-        // return result;
-        return (Node) super.clone();
-    }
-
+    
     public Node getRandomNeighbours() {
         if (neighbours.size() == 0)
             return null;
